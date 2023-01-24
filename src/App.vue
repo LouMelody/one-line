9512e311625997bfd9922cd92df061d3a0abdd95
LouMelody <x21j030@chiba-fjb.ac.jp>
2023-01-24 15:54:48 +0900

    add csv

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
 <script lang="ts" setup>
+import { board } from "@/scripts/index.ts";
+for(let line of board) {
+    console.log(line);
+}
 </script>
 
 <template>
--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -5,4 +5,16 @@ for(let i = 0; i < SIZE; i++) {
     board[i] = new Array(SIZE).fill(0);
 }
 
-export {};
+let csv = new XMLHttpRequest();
+csv.open("GET", "../../level.csv", false);
+csv.send(null);
+let lines = csv.responseText.split("\n");
+
+for(let y = 0; y < SIZE; y++) {
+    let line = lines[y].split(",");
+    for(let x = 0; x < SIZE; x++) {
+        board[y][x] = Number(line[x].trim());
+    }
+}
+
+export { board };
