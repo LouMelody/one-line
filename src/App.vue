ad5f51238778627635360de04aa62bd09d39b5b8
LouMelody <kamatatu_co_vm6ae@yahoo.co.jp>
2023-01-31 01:22:33 +0900

    for school

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,58 @@
 <script lang="ts" setup>
-import { board } from "@/scripts/index.ts";
-for(let line of board) {
-    console.log(line);
+import { SIZE, board } from "./scripts/index";
+import imgUrl  from "./assets/police.png";
+
+document.addEventListener("keydown", keyHandler);
+function isBlock(y: number, x: number): boolean  {
+    if(board[y][x] == 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function keyHandler(e: KeyboardEvent) {
+    switch(e.key) {
+        case "ArrowLeft":
+            console.log("left");
+        case "ArrowRight":
+            console.log("right");
+        case "ArrowUp":
+            console.log("up");
+        case "ArrowDown":
+            console.log("down");
+    }
 }
 </script>
 
 <template>
-  
+  <table border="1">
+    <tr v-for="y in [...Array(SIZE)].map((_, i) => i+1)" :key="`board`">
+        <td v-for="x in [...Array(SIZE)].map((_, j) => j+1)" :key="`line`" :class="{block: isBlock(y, x)}">
+            <img :src="imgUrl" class="imgSize">
+        </td>
+    </tr>
+  </table>
 </template>
+
+<style scoped>
+    table {
+        border-collapse: collapse;
+    }
+    td {
+        width: 5vw;
+        height: 5vh;
+    }
+    .block {
+        background-color: black;
+    }
+    .start {
+        background-color: aqua;
+    }
+    .imgSize {
+        display: block;
+        margin: auto;
+        width: 50px;
+        height: 50px;
+    }
+</style>
--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,8 +1,8 @@
 const SIZE = 5;
 
-let board = new Array(SIZE);
-for(let i = 0; i < SIZE; i++) {
-    board[i] = new Array(SIZE).fill(0);
+let board = new Array(SIZE+2);
+for(let i = 0; i <= SIZE+1; i++) {
+    board[i] = new Array(SIZE+2).fill(1);
 }
 
 let csv = new XMLHttpRequest();
@@ -10,11 +10,11 @@ csv.open("GET", "../../level.csv", false);
 csv.send(null);
 let lines = csv.responseText.split("\n");
 
-for(let y = 0; y < SIZE; y++) {
-    let line = lines[y].split(",");
-    for(let x = 0; x < SIZE; x++) {
-        board[y][x] = Number(line[x].trim());
+for(let y = 1; y <= SIZE; y++) {
+    let line = lines[y-1].split(",");
+    for(let x = 1; x <= SIZE; x++) {
+        board[y][x] = Number(line[x-1].trim());
     }
 }
 
-export { board };
+export { SIZE, board };
